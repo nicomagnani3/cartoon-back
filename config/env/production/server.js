3ce47a16847e349e53a2cde619cee866ecd18b8c
Nicolas Magnani <nmagnani@iupik.com>
2023-01-29 22:48:45 -0300

    changes config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,7 @@
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8081),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '192c1dd3cc7bb069706ee4a8f0199ca6'),
-    },
+  url: env("RENDER_EXTERNAL_URL"),
+  dirs: {
+    public: "/data/public"
   },
 });
