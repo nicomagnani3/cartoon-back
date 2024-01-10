546ff0695cb35c4fe131679456b6a47ef9c21394
Nicolas Magnani <nmagnani@iupik.com>
2024-01-10 13:07:57 -0300

    version node3

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8081),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '192c1dd3cc7bb069706ee4a8f0199ca6'),
