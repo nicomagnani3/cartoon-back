a08528a095424163077abc30f979c5c041275626
Nicolas Magnani <nmagnani@iupik.com>
2024-01-11 22:30:23 -0300

    render232

--- config/server.js
+++ config/server.js
@@ -1,14 +1,16 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://cartoon-tag.onrender.com'),
+  cron: {
+    enabled: false,
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '192c1dd3cc7bb069706ee4a8f0199ca6'),
     },
-    path: '/admin',
+    path: '/dashboard',
     build: {
-      backend: env('ADMIN_BUILD_BACKEND', 'https://cartoon-tag.onrender.com/admin'),
+      backend: 'https://cartoon-tag.onrender.com/admin',
     },
     autoopen: false,
 
