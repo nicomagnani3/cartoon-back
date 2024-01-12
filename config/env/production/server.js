34cfa0d8ff71d6f5fa5c6c8a66c1691c0d47403a
Nicolas Magnani <nmagnani@iupik.com>
2024-01-11 22:18:29 -0300

    render22

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,17 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://cartoon-tag.onrender.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '192c1dd3cc7bb069706ee4a8f0199ca6'),
     },
-    url: '/',
+    path: '/admin',
+    build: {
+      backend: env('ADMIN_BUILD_BACKEND', 'https://cartoon-tag.onrender.com/admin'),
+    },
+    autoopen: false,
+
 
   },
 });
