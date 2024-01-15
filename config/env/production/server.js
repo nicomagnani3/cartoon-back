72903e53210b44108c0ed9b6dc6f917a9ece888e
Nicolas Magnani <nmagnani@iupik.com>
2024-01-15 17:52:41 -0300

    revert config server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,19 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  cron: {
-    enabled: false,
-  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '192c1dd3cc7bb069706ee4a8f0199ca6'),
     },
-    path: '/dashboard',
-    build: {
-      backend: 'https://cartoon-tag.onrender.com/admin',
-    },
-    autoopen: false,
-
-
   },
 });
